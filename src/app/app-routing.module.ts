d7aae775ccb105f56184f02e30b46cc3de36b95c
Paxton Eicher <paxton.w.eicher@gmail.com>
2019-02-17 11:19:13 -0500

    Complete lazyloading, routing and search bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,37 @@ import { HomeComponent } from './modules/home/home.component';
 const routes: Routes = [
   {
     path: '',
-    children: [
-      {
-        path: 'home',
-        loadChildren: './modules/home/home.module#HomeModule'
-      },
-      {
-        path: 'standings',
-        loadChildren: './modules/standings/standings.module#StandingsModule'
-      }
-    ]
+    component: HomeComponent
   },
-];
+  {
+    path: 'pool-generator',
+    loadChildren: './modules/pool-generator/pool-generator.module#PoolGeneratorModule'
+  },
+  {
+    path: 'stats',
+    loadChildren: './modules/stats/stats.module#StatsModule'
+  },
+  {
+    path: 'standings',
+    loadChildren: './modules/standings/standings.module#StandingsModule'
+  },
+  {
+    path: 'schedule',
+    loadChildren: './modules/schedule/schedule.module#ScheduleModule'
+  },
+  {
+    path: 'players',
+    loadChildren: './modules/players/players.module#PlayersModule'
+  },
+  {
+    path: 'injuries',
+    loadChildren: './modules/injuries/injuries.module#InjuriesModule'
+  },
+  {
+    path: 'teams',
+    loadChildren: './modules/teams/teams.module#TeamsModule'
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
